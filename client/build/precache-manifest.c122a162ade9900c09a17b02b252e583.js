cde601385448203c267123b3333a7460cfd3fc81
aman2698 <71030460+aman2698@users.noreply.github.com>
2020-10-09 14:22:46 +0530

    Add files via upload

---
+++ client/build/precache-manifest.c122a162ade9900c09a17b02b252e583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f392085b5f6483e59b396bf546482e69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "209ae262928c3441d606",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "7614d711da2450e59411",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "209ae262928c3441d606",
+    "url": "/static/js/2.a9d74903.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a9d74903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7614d711da2450e59411",
+    "url": "/static/js/main.6572401b.chunk.js"
+  },
+  {
+    "revision": "a971c887e8c9d2929948",
+    "url": "/static/js/runtime-main.184ad0ad.js"
+  }
+]);
